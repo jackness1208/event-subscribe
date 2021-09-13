809c9c55fb357a1591333875d64ba9649e25df48
liudaojie <liudaojie@yy.com>
2021-09-13 23:03:36 +0800

    feat: deving

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,21 @@
 /** 事件 hooks */
 export interface EventResultMap {
-  [eventName: string]: any
-}
-
-export interface EventFnMap {
-  [eventName: string]: ((rs: any) => void)[]
-}
-
-let eventResultMap: EventResultMap = {}
-let eventFnMap: EventFnMap = {}
-
-/** 事件 key map */
-const eventKeyMap = new Map<string, EventCallback>()
-
-/** 事件key */
-let eventKeyPadding = 0
-/** 格式化 事件key */
-function formatEventKey(name: string, fnKey?: string) {
-  if (fnKey) {
-    return `${fnKey}`
-  } else {
-    return `${name}-${eventKeyPadding++}`
-  }
+  [eventName: string | number | symbol]: any
 }
 
 /** 事件回调 */
 export type EventCallback<R = any> = (rs: R) => void
 
-export const eventSubscribe = {
+export interface EventFnMap {
+  [eventName: string]: EventCallback[]
+}
+export class EventSubscribe<
+  M extends EventResultMap = EventResultMap,
+  K extends keyof M = keyof M
+> {
+  private eventResultMap: Record<K, M[K]> = {}
+  private eventFnMap: EventFnMap = {}
+
   /**
    * 事件订阅
    * @param name: 事件名称
@@ -36,7 +24,8 @@ export const eventSubscribe = {
    * @param fnKey: 用于去掉订阅时标识
    * @returns eventKey 订阅标识, 用于 off
    * */
-  on<R = any>(name: string, callback: EventCallback<R>, immediate?: boolean, fnKey?: string) {
+  on<K extends keyof M, R = M[K]>(name: K, callback: EventCallback<R>, fnKey?: string) {
+    const { eventFnMap, eventResultMap } = this
     if (name in eventFnMap) {
       eventFnMap[name].push(callback)
     } else {
@@ -56,81 +45,5 @@ export const eventSubscribe = {
       callback(eventResultMap[name])
     }
     return eventKey
-  },
-
-  /**
-   * 事件一次性订阅
-   * @param name: 事件名称
-   * @param callback: 回调方法
-   * @returns eventKey 订阅标识, 用于 off
-   * */
-  once<R = any>(name: string, callback: EventCallback<R>) {
-    const key = this.on<R>(
-      name,
-      (res) => {
-        this.off(name, key)
-        callback(res)
-      },
-      false,
-      formatEventKey(name)
-    )
-    return key
-  },
-  /**
-   * 事件退订
-   * @param name: 事件名称
-   * @param ctx: 订阅时方法 | 订阅标识
-   * */
-  off<R = any>(name: string, ctx: EventCallback<R> | string) {
-    const eventFns = eventFnMap[name]
-    let rFn: EventCallback | undefined
-    if (eventFns?.length) {
-      if (typeof ctx === 'string') {
-        rFn = eventKeyMap.get(ctx)
-      } else {
-        rFn = ctx
-      }
-
-      if (rFn) {
-        const rFnIndex = eventFns.indexOf(rFn)
-
-        if (rFnIndex !== -1) {
-          eventFns.splice(rFnIndex, 1)
-        }
-      }
-    }
-  },
-
-  /**
-   * 事件广播
-   * @param name: 事件名称
-   * @param data: 入参数据
-   * */
-  trigger<R = any>(name: string, data: R) {
-    if (name in eventFnMap) {
-      eventFnMap[name].forEach((fn) => {
-        fn(data)
-      })
-    }
-    eventResultMap[name] = data
-  },
-  /**
-   * 事件回放
-   * @param name: 事件名称
-   * */
-  replay(name: string) {
-    if (name in eventFnMap && name in eventResultMap) {
-      const lastResult = eventResultMap[name]
-      eventFnMap[name].forEach((fn) => {
-        fn(lastResult)
-      })
-    }
-  },
-
-  /** reset 清空已绑定的事件 */
-  reset() {
-    eventResultMap = {}
-    eventFnMap = {}
-    eventKeyMap.clear()
   }
 }
