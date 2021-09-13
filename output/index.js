6748e5176a6506e44f0c0811edb1c6ab7d786cf9
liudaojie <liudaojie@yy.com>
2021-09-14 00:24:27 +0800

    Auto stash before merge of "master" and "1.0.0"

--- output/index.js
+++ output/index.js
@@ -3,4 +3,121 @@
  * (c) 2020 - 2021 jackness
  * Released under the MIT License.
  */
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var t=function(){function t(){this.eventResultMap={},this.eventFnMap={},this.eventKeyMap=new Map,this.eventKeyPadding=0}return t.prototype.formatEventKey=function(t,e){return e?""+e:t+"-"+this.eventKeyPadding++},t.prototype.on=function(t,e,n,o){var i,r=this,s=r.eventFnMap,p=r.eventResultMap,v=r.eventKeyMap;s[t]?null===(i=s[t])||void 0===i||i.push(e):s[t]=[e],o&&this.off(t,o);var a=this.formatEventKey(""+t,o);return v.set(a,e),n&&t in p&&e(p[t]),a},t.prototype.onceUntil=function(t,e,n){var o=this,i=this.on(t,(function(n){e(n)||o.off(t,i)}),n,this.formatEventKey(""+t));return i},t.prototype.once=function(t,e){var n=this,o=this.on(t,(function(i){n.off(t,o),e(i)}),!1,this.formatEventKey(""+t));return o},t.prototype.off=function(t,e){var n,o=this.eventFnMap,i=this.eventKeyMap,r=o[t];if((null==r?void 0:r.length)&&(n="string"==typeof e?i.get(e):e)){var s=r.indexOf(n);-1!==s&&r.splice(s,1)}},t.prototype.trigger=function(t,e){var n,o=this.eventFnMap,i=this.eventResultMap;o[t]&&(null===(n=o[t])||void 0===n||n.forEach((function(t){t(e)}))),i[t]=e},t.prototype.replay=function(t){var e,n=this.eventFnMap,o=this.eventResultMap;if(n[t]&&t in o){var i=o[t];null===(e=n[t])||void 0===e||e.forEach((function(t){t(i)}))}},t.prototype.reset=function(){this.destroy()},t.prototype.destroy=function(){this.eventResultMap={},this.eventFnMap={},this.eventKeyMap.clear()},t}(),e=new t;exports.EventSubscribe=t,exports.eventSubscribe=e;
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var eventResultMap = {};
+var eventFnMap = {};
+/** 事件 key map */
+var eventKeyMap = new Map();
+/** 事件key */
+var eventKeyPadding = 0;
+/** 格式化 事件key */
+function formatEventKey(name, fnKey) {
+    if (fnKey) {
+        return "" + fnKey;
+    }
+    else {
+        return name + "-" + eventKeyPadding++;
+    }
+}
+var eventSubscribe = {
+    /**
+     * 事件订阅
+     * @param name: 事件名称
+     * @param callback: 回调方法
+     * @param immediate: 若订阅之前已经触发过，是否马上执行
+     * @param fnKey: 用于去掉订阅时标识
+     * @returns eventKey 订阅标识, 用于 off
+     * */
+    on: function (name, callback, immediate, fnKey) {
+        if (name in eventFnMap) {
+            eventFnMap[name].push(callback);
+        }
+        else {
+            eventFnMap[name] = [callback];
+        }
+        if (fnKey) {
+            // 查看是否之前已经有绑定, 有则先去掉
+            eventSubscribe.off(name, fnKey);
+        }
+        // key 关系初始化
+        var eventKey = formatEventKey(name, fnKey);
+        eventKeyMap.set(eventKey, callback);
+        if (immediate && name in eventResultMap) {
+            callback(eventResultMap[name]);
+        }
+        return eventKey;
+    },
+    /**
+     * 事件一次性订阅
+     * @param name: 事件名称
+     * @param callback: 回调方法
+     * @returns eventKey 订阅标识, 用于 off
+     * */
+    once: function (name, callback) {
+        var _this = this;
+        var key = this.on(name, function (res) {
+            _this.off(name, key);
+            callback(res);
+        }, false, formatEventKey(name));
+        return key;
+    },
+    /**
+     * 事件退订
+     * @param name: 事件名称
+     * @param ctx: 订阅时方法 | 订阅标识
+     * */
+    off: function (name, ctx) {
+        var eventFns = eventFnMap[name];
+        var rFn;
+        if (eventFns === null || eventFns === void 0 ? void 0 : eventFns.length) {
+            if (typeof ctx === 'string') {
+                rFn = eventKeyMap.get(ctx);
+            }
+            else {
+                rFn = ctx;
+            }
+            if (rFn) {
+                var rFnIndex = eventFns.indexOf(rFn);
+                if (rFnIndex !== -1) {
+                    eventFns.splice(rFnIndex, 1);
+                }
+            }
+        }
+    },
+    /**
+     * 事件广播
+     * @param name: 事件名称
+     * @param data: 入参数据
+     * */
+    trigger: function (name, data) {
+        if (name in eventFnMap) {
+            eventFnMap[name].forEach(function (fn) {
+                fn(data);
+            });
+        }
+        eventResultMap[name] = data;
+    },
+    /**
+     * 事件回放
+     * @param name: 事件名称
+     * */
+    replay: function (name) {
+        if (name in eventFnMap && name in eventResultMap) {
+            var lastResult_1 = eventResultMap[name];
+            eventFnMap[name].forEach(function (fn) {
+                fn(lastResult_1);
+            });
+        }
+    },
+    /** reset 清空已绑定的事件 */
+    reset: function () {
+        eventResultMap = {};
+        eventFnMap = {};
+        eventKeyMap.clear();
+    }
+};
+
+exports.eventSubscribe = eventSubscribe;
