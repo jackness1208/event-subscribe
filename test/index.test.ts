2bd12112fdc5e5146fbaf020ba8de64ba0adc7ff
liudaojie <liudaojie@yy.com>
2021-09-19 23:08:20 +0800

    feat: 补充 `once(key, fn, immediate)` 中 `immediate` 属性

--- test/index.test.ts
+++ test/index.test.ts
@@ -154,6 +154,22 @@ test('eventSubscribe.once() test', () => {
   expect(result).toEqual(['a-01'])
 })
 
+test('eventSubscribe.once() immediate test', () => {
+  eventSubscribe.reset()
+  const result: string[] = []
+  eventSubscribe.trigger('hello', '01')
+  const key = eventSubscribe.once(
+    'hello',
+    (ctx) => {
+      result.push(`a-${ctx}`)
+    },
+    true
+  )
+
+  eventSubscribe.trigger('hello', '02')
+  expect(result).toEqual(['a-01'])
+})
+
 test('eventSubscribe.onceUntil() test', () => {
   eventSubscribe.reset()
   const result: string[] = []
