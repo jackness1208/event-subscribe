0f66d6093ed4bf64da41c68556bda05c25bfa795
liudaojie <liudaojie@yy.com>
2025-05-19 10:36:04 +0800

    feat: update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,16 @@
 import pkg from './package.json'
 import typescript from 'rollup-plugin-typescript2'
-import commonjs from 'rollup-plugin-commonjs'
-import nodeResolve from 'rollup-plugin-node-resolve'
-import json from 'rollup-plugin-json'
-import { terser } from 'rollup-plugin-terser'
+import commonjs from '@rollup/plugin-commonjs'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
+import json from '@rollup/plugin-json'
+import buble from '@rollup/plugin-buble'
+import replace from '@rollup/plugin-replace'
+import path from 'path'
+import fs from 'fs'
+const dts = require('rollup-plugin-dts')
+
+const IS_PRODUCTION = process.env.NODE_ENV === 'production'
 
 function buildBanner(type) {
   return [
@@ -15,55 +22,59 @@ function buildBanner(type) {
   ].join('\n')
 }
 
-const IS_PUBLISH = process.env.NODE_ENV === 'production'
+const srcRoot = path.join(__dirname, 'src')
+const files = fs.readdirSync(srcRoot).filter((name) => {
+  if (path.extname(name) === '.ts') {
+    return true
+  } else {
+    return false
+  }
+})
 
-const config = {
-  input: './src/index.ts',
-  output: [],
-  plugins: [
-    nodeResolve({ jsnext: true }),
-    commonjs(),
-    json(),
-    typescript({
-      typescript: require('typescript')
-    })
-  ],
-  external: ['@yy/allblue-qiankun', 'fetch-polyfill']
-}
-
-export default [
-  {
-    input: config.input,
+export default files.map((filename) => {
+  const name = filename.replace(/\.[^.]+$/, '')
+  return {
+    input: path.join('./src', filename),
+    onwarn(warning) {
+      if (['CIRCULAR_DEPENDENCY', 'EVAL'].includes(warning.code)) {
+        // nothing
+      } else {
+        console.warn(warning.message)
+      }
+    },
     output: [
       {
-        file: './output/index.js',
+        file: path.join('./output', `${name}.js`),
         format: 'cjs',
-        banner: buildBanner('cjs'),
+        banner: buildBanner('js'),
         exports: 'named',
         sourcemap: false
-      }
-    ],
-    plugins: config.plugins.concat([
-      IS_PUBLISH &&
-        terser({
-          compress: {
-            passes: 2
-          }
-        })
-    ]),
-    external: config.external
-  },
-  {
-    input: config.input,
-    output: [
+      },
       {
-        file: './output/index.esm.js',
+        file: path.join('./output', `${name}.esm.js`),
         format: 'esm',
         banner: buildBanner('esm'),
         sourcemap: false
       }
     ],
-    plugins: config.plugins,
-    external: config.external
+    plugins: [
+      nodeResolve({ mainFields: ['module', 'main', 'jsnext:main'] }),
+      commonjs({
+        include: [/node_modules/, /src\/proto/]
+      }),
+      json(),
+      typescript({
+        clean: true,
+        typescript: require('typescript')
+      }),
+      replace({
+        preventAssignment: true,
+        values: {
+          'process.env.version': `'${pkg.version}'`
+        }
+      }),
+      buble({ objectAssign: 'Object.assign' })
+    ].concat(IS_PRODUCTION ? [terser()] : []),
+    external: []
   }
-]
+})
